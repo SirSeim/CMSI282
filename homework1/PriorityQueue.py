bf0c96eb47bccf2fc37ef246dbc40f4df538cf07
Edward Seim <sirseim@gmail.com>
2015-02-10 01:22:33 -0800

    PriorityQueue v1 complete
    Fully working implementation

--- homework1/PriorityQueue.py
+++ homework1/PriorityQueue.py
@@ -1,26 +1,22 @@
-import math
-
 class PriorityQueue:
     def __init__ (self):
         self.array = []
 
     def add (self, value):
-        if len(self) < 2:
-            self.array[0] = None
-            self.array[1] = value
+        if len(self.array) < 2:
+            self.array.append(None)
+            self.array.append(value)
         else:
             self.array.append(value)
         counter = len(self.array) - 1
         while counter > 1:
-            half_counter = math.floor(counter / 2)
+            half_counter = counter / 2
             if self.array[counter] > self.array[half_counter]:
-                replacement = self.array[half_counter]
-                self.array[half_counter] = self.array[counter]
-                self.array[counter] = replacement
+                self.switch(counter, half_counter)
             counter = half_counter
 
     def peek (self):
-        if len(self) < 2:
+        if len(self.array) < 2:
             return None
         else:
             return self.array[1]
@@ -28,20 +24,41 @@ class PriorityQueue:
     def remove (self):
         result = self.peek()
         if result is not None:
-            self.array[1] = self.array.pop(len(self.array) - 1)
+            self.switch(1, len(self.array) - 1)
+            self.array.pop(len(self.array) - 1)
+
             counter = 1
-            while counter * 2 <= len(self.array):
-                first_child = counter * 2
-                second_child = counter * 2 + 1
-                if second_child 
-                if self.array[first_child] > self.array[second_child]:
+            while counter * 2 <= len(self.array) - 1:
+                child = counter * 2
+                if child < len(self.array) - 1 and self.array[child] < self.array[child + 1]:
+                    child += 1
+                if not self.array[counter] < self.array[child]:
+                    break
+                self.switch(counter, child)
+                counter = child
                     
         return result
 
+    def switch (self, first, second):
+        replacement = self.array[first]
+        self.array[first] = self.array[second]
+        self.array[second] = replacement
+
     def __len__ (self):
-        return len(self.array)
+        return len(self.array) - 1
 
     def __str__ (self):
-        return "This is a PriorityQueue"
+        print_queue = PriorityQueue()
+        for value in self.array[1:]:
+            print_queue.add(value)
 
-
+        end = len(print_queue)
+        result = "["
+        for index in range(0, end):
+            result += "\'"
+            result += str(print_queue.remove())
+            result += "\'"
+            if index < end - 1 :
+                result += ", "
+        result += "]"
+        return result
