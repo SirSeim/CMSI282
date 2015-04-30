9a11a624f11db83682422484458089fd75a7beb1
Ray Toal <rtoal@lmu.edu>
2015-04-29 19:35:12 -0700

    Make bozosort a little more Pythonic

--- homework3/Bozosort.py
+++ homework3/Bozosort.py
@@ -3,16 +3,16 @@ import time
 
 def bozo_sort (inputs):
     array = list(inputs)
-    def swap (array, index1, index2):
+    def swap(array, index1, index2):
         array[index1], array[index2] = array[index2], array[index1]
 
-    def sorted (array):
+    def is_sorted (array):
         for index in range(0, len(array) - 1):
             if (array[index] > array[index + 1]):
                 return False
         return True
 
-    while (not sorted(array)):
+    while not is_sorted(array):
         int1 = randint(0, len(array) - 1)
         int2 = randint(0, len(array) - 1)
         swap(array, int1, int2)
@@ -22,7 +22,7 @@ def bozo_sort (inputs):
 wip = [10,9,8,7,6,5,4,3,2,1]
 trials = []
 
-while (len(trials) < 10):
+while len(trials) < 10:
     start = time.time()
     bozo_sort(wip)
     end = time.time()
